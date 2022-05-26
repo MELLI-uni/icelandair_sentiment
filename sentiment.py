9ce884526edca6848136af1f2d1672aeef98d145
MELLI <jae0228@gmail.com>
2022-05-26 17:50:55 +0000

    initialization

---
+++ sentiment.py
@@ -0,0 +1,19 @@
+import nltk
+from nltk import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+import numpy as np
+import pandas as pd
+
+import Functions
+
+eng_file_name = './Data/NLP_English_JAN2022_OPEN.xlsx'
+eng_sheet_name = 'Result 1'
+
+df = Functions.init(eng_file_name, eng_sheet_name)
+
+df = Functions.clean(df)
+
+#print(df)
+
+Functions.process(df)
