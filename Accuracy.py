80912233b22ed4a0a05e4039b4c9f04a09555722
MELLI <jae0228@gmail.com>
2022-05-30 15:20:12 +0000

    created accuracy.py file

---
+++ Accuracy.py
@@ -0,0 +1,4 @@
+# TASK7: Create an accuracy file that returns F1, precision, recall value in a table format when inputted two files (testing and actual)
+
+def accuracy(test_file, actual_file):
+    print("accuracy test")
