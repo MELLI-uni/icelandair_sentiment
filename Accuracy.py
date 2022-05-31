d296969e910030abe3b1255c52e75fdb7dea1c5f
MELLI <jae0228@gmail.com>
2022-05-31 16:58:58 +0000

    Starting score calculation

--- Accuracy.py
+++ Accuracy.py
@@ -1,4 +1,8 @@
 # TASK7: Create an accuracy file that returns F1, precision, recall value in a table format when inputted two files (testing and actual)
 
-def accuracy(test_file, actual_file):
+def accuracy(actual_file, test_file):
+    # Precision Score = TP / (FP + TP)
+    # Recall Score = TP / (FN + TP)
+    # Accuracy Score = (TP + TN) / (TP + FN + TN + FP)
+    # F1 Score = 2* Precision Score * Recall Score / (Precision Score + Recall Score)
     print("accuracy test")
--- sentiment.py
+++ sentiment.py
@@ -6,13 +6,21 @@ import numpy as np
 import pandas as pd
 
 import Functions
+import Accuracy
 
 eng_file_name = './Data/NLP_English_JAN2022_OPEN.xlsx'
 eng_sheet_name = 'Result 1'
 
-df = Functions.init(eng_file_name, eng_sheet_name)
+#df = Functions.init(eng_file_name, eng_sheet_name)
 
-df = Functions.clean(df)
+#df = Functions.clean(df)
 
-Functions.process(df)
-print(df)
+#Functions.process(df)
+
+test_actual = "dummy_actual.txt"
+test_correct = "dummy_correct.txt"
+test_incorrect = "dummy_incorrect.txt"
+
+Accuracy.accuracy(test_actual, test_correct)
+
+#print(df)
