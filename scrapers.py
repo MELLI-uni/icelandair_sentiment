2ef323cb36e250de1e6366e30c08f98c621ea223
MELLI <jae0228@gmail.com>
2022-06-02 11:42:42 +0000

    scraper initialization

---
+++ scrapers.py
@@ -0,0 +1,5 @@
+# TASK2-C: Create a destination scraper
+# Scraping from: https://aviationvoice.com/did-you-know-how-new-aircraft-names-are-created-201907151835/#:~:text=Boeing%20Principle&text=The%20most%20of%20Boeing%20aircraft,7X7%20scheme%20was%20Boeing%20707.
+
+# TASK2-B1: Create an emoji-scraper
+# Scraping from: https://emojipedia.org/
