eaafdff8100543e4d88226c9c551e8ea24da8fbc
MELLI <jae0228@gmail.com>
2022-06-02 10:43:59 +0000

    completed TASK7, organized folders, added Tester_Files folder to place files for testing

--- Accuracy.py
+++ Accuracy.py
@@ -1,12 +1,63 @@
-# TASK7: Create an accuracy file that returns F1, precision, recall value in a table format when inputted two files (testing and actual)
+import numpy as np
+import pandas as pd
+from tabulate import tabulate
 
-<<<<<<< HEAD
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import f1_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import accuracy_score
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
 def accuracy(actual_file, test_file):
-    # Precision Score = TP / (FP + TP)
-    # Recall Score = TP / (FN + TP)
-    # Accuracy Score = (TP + TN) / (TP + FN + TN + FP)
-    # F1 Score = 2* Precision Score * Recall Score / (Precision Score + Recall Score)
-=======
-def accuracy(test_file, actual_file):
->>>>>>> 80912233b22ed4a0a05e4039b4c9f04a09555722
-    print("accuracy test")
+    """
+    accuracy function displays the confusion matrix and calculates precision, recall, f1, f1 micro-average, f1 macro-average scores, and prints it in a tabular format
+
+    : param actual_file: xlsx file with the truth data
+    : param test_file: xlsx file with the predicted data
+
+    : return: N/A
+    """
+    # Open the two files and convert Sentiment column to list
+    df_truth = pd.read_excel(actual_file)
+    senti_truth = df_truth['Sentiment'].tolist()
+    labels = np.unique(senti_truth)
+
+    df_predict = pd.read_excel(test_file)
+    senti_predict = df_predict['Sentiment'].tolist()
+
+    # Generate confusion matrix
+    cf_matrix = confusion_matrix(senti_truth, senti_predict, labels=labels)
+    df_matrix = pd.DataFrame(cf_matrix, index=labels, columns=labels)
+
+    ax = sns.heatmap(df_matrix, annot=True, cmap='Blues', linecolor='white', cbar='True', xticklabels='auto', yticklabels='auto')
+    ax.set(title = "Confusion Matrix",
+            xlabel = "Predicted Sentiments",
+            ylabel = "Actual Sentiments")
+    sns.set(font_scale=0.7)
+
+    plt.show()
+
+    # Calculate precision, recall, accuracy
+    precision = precision_score(senti_truth, senti_predict, labels = labels, average = None)
+    recall = recall_score(senti_truth, senti_predict, labels = labels, average = None)
+    #accuracy = accuracy_score(senti_truth, senti_predict, labels = labels)
+
+    # Calculate f1 score
+    f1_gen = f1_score(senti_truth, senti_predict, labels = labels, average = None)
+    # Micro average f1 -> calculates positive and negative values globally
+    f1_micro = f1_score(senti_truth, senti_predict, labels = labels, average='micro')
+    # Macro average f1 -> takes the average of each class's F1 score
+    f1_macro = f1_score(senti_truth, senti_predict, labels = labels, average='macro')
+
+    # Compile scores in panda dataframe
+    score_compile = np.array([precision, recall, f1_gen])
+    f1_average = np.array([f1_micro, f1_macro])
+    df_score = pd.DataFrame(data=score_compile, index=['Precision', 'Recall', 'F1'], columns=labels)
+    df_average = pd.DataFrame(data=f1_average, index=['F1 Microaverage', 'F1 Macroaverage'], columns=['Scores'])
+
+    # Print dataframe in tabular format
+    print(tabulate(df_score, headers='keys', tablefmt='pretty'))
+    print(tabulate(df_average, headers='keys', tablefmt='pretty'))
--- sentiment.py
+++ sentiment.py
@@ -11,16 +11,20 @@ import Accuracy
 eng_file_name = './Data/NLP_English_JAN2022_OPEN.xlsx'
 eng_sheet_name = 'Result 1'
 
+isk_file_name = './NLP_Icelandic_14FEB2022_YTD.xlsx'
+isk_sheet_name = 'Result 1'
+
 #df = Functions.init(eng_file_name, eng_sheet_name)
 
 #df = Functions.clean(df)
 
 #Functions.process(df)
 
-test_actual = "dummy_actual.txt"
-test_correct = "dummy_correct.txt"
-test_incorrect = "dummy_incorrect.txt"
+# test_actual = "./Tester_Files/dummy_actual.xlsx"
+# test_correct = "./Tester_Files/dummy_correct.xlsx"
+# test_incorrect = "./Tester_Files/dummy_incorrect.xlsx"
 
-Accuracy.accuracy(test_actual, test_correct)
+# 
+# Accuracy.accuracy(test_actual, test_incorrect)
 
 #print(df)
