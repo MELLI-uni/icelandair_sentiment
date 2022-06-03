4c40ff4d6e8a247fc3e0450cc22a661bc096765d
MELLI <jae0228@gmail.com>
2022-06-03 14:06:17 +0000

    initialization

---
+++ update-lexicons.py
@@ -0,0 +1,11 @@
+# TASK2-D: Create an update-lexicon py file that will create eng-lexicons based on the leftover words
+def eng_update():
+    print("English update")
+
+# TASK2-E: Create an update-lexicon py file that will create eng-lexicons based on the leftover words
+def isk_update():
+    print("Icelandic update")
+
+
+eng_update()
+isk_update()
