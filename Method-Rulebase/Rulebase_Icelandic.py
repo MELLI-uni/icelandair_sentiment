105cc5f3cdf3c3e8cd22f83605898945a5af8464
MELLI <jae0228@gmail.com>
2022-07-10 03:12:02 +0000

    added emoji dictionary

--- Method-Rulebase/Rulebase_Icelandic.py
+++ Method-Rulebase/Rulebase_Icelandic.py
@@ -3,6 +3,8 @@ import numpy as np
 import pandas as pd
 pd.set_option('mode.chained_assignment', None)
 
+from itertools import islice
+
 from reynir import Greynir
 from reynir_correct import check
 
@@ -12,7 +14,11 @@ isk_greynir = Greynir()
 translator = Translator(to_lang="is")
 
 emoji_dict = {}
-
-
-translation = translator.translate("this is a pen")
-print(translation)
+with open('../lexicons/emoji.txt', encoding='utf-8') as f:
+    for line in islice(f, 1, None):
+        [key, value] = line.split("\t")
+        emoji_dict[key] = translator.translate(value).strip()
+with open('../lexicons/emoticon.txt', encoding='utf-8') as f:
+    for line in f:
+        [key, value] = line.split("\t")
+        emoji_dict[key] = translator.translate(value).strip()
