6a96b1a2af48903fe5d3a1ca010b9ed91ab0f4a6
MELLI <jae0228@gmail.com>
2022-07-10 00:58:17 +0000

    modified datainfo file

--- DataInfo.py
+++ DataInfo.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 labeled_english = './eng_total.pkl'
 labeled_icelandic = './isk_total.pkl'
-unlabled = './tuning_total.pkl'
+unlabeled = './tuning_total.pkl'
 unlabeled_english = 'Tuning/tuning_eng.pkl'
 unlabeled_icelandic = 'Tuning/tuning_isk.pkl'
 unlabeled_other = 'Tuning/tuning_other.pkl'
@@ -11,23 +11,29 @@ print("+-----------------------------------+")
 print("|INFORMATION ON LABELED ENGLISH DATA|")
 print("+-----------------------------------+")
 df_labeled_english = pd.read_pickle(labeled_english)
-print("|total: ", df_labeled_english.shape[0])
-print("|positive: ")
-print("|neutral: ")
-print("|negative: ")
-print("\n\n")
+print("|total: ", df_labeled_english.shape[0], "|")
+print("|positive: ", "|")
+print("|neutral: ", "|")
+print("|negative: ", "|")
+print("+-----------------------------------+")
+print("\n")
 
-print("INFORMATION ON LABELED ICELANDIC DATA")
-print("-------------------------------------")
-print("total: ")
-print("positive: ")
-print("negative: ")
-print("\n\n")
+print("+-------------------------------------+")
+print("|INFORMATION ON LABELED ICELANDIC DATA|")
+print("+-------------------------------------+")
+df_labeled_icelandic = pd.read_pickle(labeled_icelandic)
+print("|total: ", df_labeled_icelandic.shape[0], "|")
+print("|positive: ", "|")
+print("|neutral: ", "|")
+print("|negative: ", "|")
+print("+-------------------------------------+")
+print("\n")
 
-print("INFORMATION ON UNLABELED DATA")
-print("-----------------------------")
-print("total: ")
-print("english: ")
-print("icelandic: ")
-print("others: ")
-print("\n\n")
+print("+-----------------------------+")
+print("|INFORMATION ON UNLABELED DATA|")
+print("+-----------------------------+")
+print("|total: ", pd.read_pickle(unlabeled).shape[0], "|")
+print("|english: ", pd.read_pickle(unlabeled_english).shape[0], "|")
+print("|icelandic: ", pd.read_pickle(unlabeled_icelandic).shape[0], "|")
+print("|others: ", pd.read_pickle(unlabeled_other).shape[0], "|")
+print("+-----------------------------+")
