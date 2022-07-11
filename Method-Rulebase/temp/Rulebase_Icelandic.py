15612912df082f1051727aeea195492cd98fd54d
MELLI <jae0228@gmail.com>
2022-07-11 17:19:30 +0000

    added pickle dictionary set

--- Method-Rulebase/temp/Rulebase_Icelandic.py
+++ Method-Rulebase/temp/Rulebase_Icelandic.py
@@ -1,19 +1,28 @@
 import regex as re
 import numpy as np
+import pickle
 import pandas as pd
 pd.set_option('mode.chained_assignment', None)
 
 from itertools import islice
 
 from reynir import Greynir
-from reynir_correct import check_single
+from reynir_correct import tokenize
 
 g = Greynir()
 
-isk_stop = []
-#with open('../../lexicons/isk_stop.txt', encoding='utf-8') as f:
-#    for line in f:
-#        isk_stop.append(line.strip())
+# Load all dictionaries
+# Pickle file contains
+#   0. English emoji dictionary
+#   1. Icelandic emoji dictionary
+#   2. Flight and destination list
+#   3. Icelandic stopwords list
+with open('dictionaries.pickle', 'rb') as handle:
+    dictionaries = pickle.load(handle)
+
+emoji_dict = dictionaries[1]
+flight_dict = dictionaries[2]
+isk_stop = dictionaries[3]
 
 def data_cleaning(df):
     # Remove na values from dataframe
@@ -25,12 +34,21 @@ def data_cleaning(df):
 
     sentences = []
 
+    counter = 0
+
     for line in df.values:
-        j = g.submit(line[0])
-        for pg in j.paragraphs():
+        counter += 1
+
+        if(counter == 10):
+            break
+
+        job = g.submit(line[0])
+        for pg in job.paragraphs():
             for sent in pg:
-                sentences.append(sent.tidy_text)
-                sent.parse()
+                #sentences.append(sent.tidy_text)
+                t = tokenize(str(sent))
+                tokens = [item.txt.lower() for item in t if item.txt != '']
+                sentences.append(tokens)
 
     return sentences
 
@@ -38,5 +56,5 @@ df_train = pd.read_pickle('./isk_train.pkl')
 df_test = pd.read_pickle('./isk_test.pkl')
 df_unlabeled = pd.read_pickle('./tuning_isk.pkl')
 
-sentences_list = data_cleaning(df_unlabeled)
-print(sentences_list[0])
+# sentences_list = data_cleaning(df_unlabeled)
+# print(sentences_list)
