c919d898afa6d0d3e36f2d981ec01747b212a88f
MELLI <jae0228@gmail.com>
2022-07-11 17:33:32 +0000

    updated dictionary

--- Method-Rulebase/temp/SaveDicts.py
+++ Method-Rulebase/temp/SaveDicts.py
@@ -1,39 +1,47 @@
 import pickle
 from itertools import islice
-from translate import Translator
+# from translate import Translator
 
-translator = Translator(to_lang="is")
+# translator = Translator(to_lang="is")
 
 dictionaries  = []
 
 # Make English emoji dictionary from emoticon and emoji file
 # Make Icelandic emoji dictionary by translating English into Icelandic (with translate api)
-emoji_dict_eng = {}
-emoji_dict_isk = {}
-with open('../../lexicons/emoji.txt', encoding='utf-8') as f:
-    for line in islice(f, 1, None):
-        [key, value] = line.split("\t")
-        emoji_dict_eng[key] = value.strip()
-        emoji_dict_isk[key] = translator.translate(value.strip())
-with open('../../lexicons/emoticon.txt', encoding='utf-8') as f:
-    for line in f:
-        [key, value] = line.split("\t")
-        emoji_dict_eng[key] = value.strip()
-        emoji_dict_isk[key] = translator.translate(value.strip())
-dictionaries.append(emoji_dict_eng)
-dictionaries.append(emoji_dict_isk)
+# emoji_dict_eng = {}
+# emoji_dict_isk = {}
+# with open('../../lexicons/emoji.txt', encoding='utf-8') as f:
+#     for line in islice(f, 1, None):
+#         [key, value] = line.split("\t")
+#         emoji_dict_eng[key] = value.strip()
+#         emoji_dict_isk[key] = translator.translate(value.strip())
+# with open('../../lexicons/emoticon.txt', encoding='utf-8') as f:
+#     for line in f:
+#         [key, value] = line.split("\t")
+#         emoji_dict_eng[key] = value.strip()
+#         emoji_dict_isk[key] = translator.translate(value.strip())
+# dictionaries.append(emoji_dict_eng)
+# dictionaries.append(emoji_dict_isk)
+
+with open('dictionaries.pickle', 'rb') as handle:
+    loader = pickle.load(handle)
+
+dictionaries.append(loader[0])
+dictionaries.append(loader[1])
 
 # Make flight and destination list from destination lexicon
 flight_list = []
 with open('../../lexicons/destination.txt', encoding='utf-8') as f:
     for line in f:
         flight_list.append(line.strip())
+dictionaries.append(flight_list)
 
 # Make icelandic stopword list from stopword lexicon
 isk_stop = []
 with open('../../lexicons/isk_stop.txt', encoding='utf-8') as f:
     for line in f:
         isk_stop.append(line.strip())
+dictionaries.append(isk_stop)
 
 # Store data (serialize)
 with open('dictionaries.pickle', 'wb') as handle:
