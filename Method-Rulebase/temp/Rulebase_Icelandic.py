78c9997e60531ea2c59a7440bfaddc18e4dff810
test <jiyoon@base>
2022-07-11 15:49:40 +0000

    update

--- Method-Rulebase/temp/Rulebase_Icelandic.py
+++ Method-Rulebase/temp/Rulebase_Icelandic.py
@@ -6,30 +6,37 @@ pd.set_option('mode.chained_assignment', None)
 from itertools import islice
 
 from reynir import Greynir
-from reynir_correct import check
+from reynir_correct import check_single
 
-isk_greynir = Greynir()
+g = Greynir()
 
 isk_stop = []
-with open('../../lexicons/isk_stop.txt', encoding='utf-8') as f:
-    for line in f:
-        isk_stop.append(line.strip())
+#with open('../../lexicons/isk_stop.txt', encoding='utf-8') as f:
+#    for line in f:
+#        isk_stop.append(line.strip())
 
 def data_cleaning(df):
-    print("Initial Data Shape: ", df.shape)
-
     # Remove na values from dataframe
     df.dropna(inplace=True)
     df.reset_index(inplace=True, drop=True)
 
     # Remove duplicate values from dataframe
-    
-    print("Final Data Shape: ", df.shape)
+    df = df.drop_duplicates()
 
-    return df
+    sentences = []
+
+    for line in df.values:
+        j = g.submit(line[0])
+        for pg in j.paragraphs():
+            for sent in pg:
+                sentences.append(sent.tidy_text)
+                sent.parse()
+
+    return sentences
 
 df_train = pd.read_pickle('./isk_train.pkl')
 df_test = pd.read_pickle('./isk_test.pkl')
 df_unlabeled = pd.read_pickle('./tuning_isk.pkl')
 
-data_cleaning(df_unlabeled)
+sentences_list = data_cleaning(df_unlabeled)
+print(sentences_list[0])
