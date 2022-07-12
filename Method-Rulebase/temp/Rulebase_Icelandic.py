ae7671d1e0abab5fe394c218f4802580110cde7b
MELLI <jae0228@gmail.com>
2022-07-12 19:39:29 +0000

    copied codes from Rulebase org

--- Method-Rulebase/temp/Rulebase_Icelandic.py
+++ Method-Rulebase/temp/Rulebase_Icelandic.py
@@ -7,6 +7,11 @@ import numpy as np
 import pandas as pd
 pd.set_option('mode.chained_assignment', None)
 
+from sklearn.metrics import f1_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from tabulate import tabulate
+
 from itertools import islice
 from gensim.models import Word2Vec
 
@@ -174,8 +179,6 @@ def process_dataframe(df):
     df.dropna(subset = ['answer_freetext_value'], inplace=True)
     df.dropna(subset = ['Sentiment'], inplace=True)
 
-    #print(df)
-
     return df
 
 def open_lexicon(path, file_name):
@@ -191,6 +194,80 @@ def open_lexicon(path, file_name):
     f.close()
 
     return lexicon, tuning
+
+def find_in_lexicon(tokens, lexicon):
+    score = []
+
+    for i in tokens:
+        if i in lexicon:
+            score.append(lexicon[i])
+            continue
+
+        score.append(0)
+
+    return score
+
+def calculate(tokens, lexicon, polarity):
+    indiv_score = find_in_lexicon(tokens, lexicon)
+
+    score = np.multiply(indiv_score, polarity)
+
+    if score > 0:
+        return 'positive'
+    elif score < 0:
+        return 'negative'
+    else:
+        return 'neutral'
+
+def label(df):
+    path = './'
+    file_name = 'isk_lexicon.txt'
+
+    lexicon, tuning = open_lexicon(path, file_name)
+
+    sentences = data_cleaning(df)
+    tags, lemmas = tag_n_lemmatize(tuple(sentences))
+
+    del df['answer_freetext_value']
+    df = df.join(pd.Series(lemmas, name='tmp_lemma'))
+    df = df.join(pd.Series(tags, name='tmp_tag'))
+
+    df = df.apply(lambda x: filtering(x['tmp_lemma'], x['tmp_tag'], x['Sentiment']), axis=1, result_type='expand')
+    df.columns = ['answer_freetext_value', 'Sentiment']
+
+    df_sentiment = df.apply(lambda x: calculate(x['answer_freetext_value'], lexicon, x['Sentiment']))
+    df['Sentiment'] = df_sentiment
+
+    return df
+
+def accuracy(df_truth, df_predict):
+    # Open the two files and convert Sentiment column to list
+    senti_truth = df_truth['Sentiment'].tolist()
+    labels = np.unique(senti_truth)
+
+    senti_predict = df_predict['Sentiment'].tolist()
+
+    # Calculate precision, recall, accuracy
+    precision = precision_score(senti_truth, senti_predict, labels = labels, average = None)
+    recall = recall_score(senti_truth, senti_predict, labels = labels, average = None)
+    #accuracy = accuracy_score(senti_truth, senti_predict, labels = labels)
+
+    # Calculate f1 score
+    f1_gen = f1_score(senti_truth, senti_predict, labels = labels, average = None)
+    # Micro average f1 -> calculates positive and negative values globally
+    f1_micro = f1_score(senti_truth, senti_predict, labels = labels, average='micro')
+    # Macro average f1 -> takes the average of each class's F1 score
+    f1_macro = f1_score(senti_truth, senti_predict, labels = labels, average='macro')
+
+    # Compile scores in panda dataframe
+    score_compile = np.array([precision, recall, f1_gen])
+    f1_average = np.array([f1_micro, f1_macro])
+    df_score = pd.DataFrame(data=score_compile, index=['Precision', 'Recall', 'F1'], columns=labels)
+    df_average = pd.DataFrame(data=f1_average, index=['F1 Microaverage', 'F1 Macroaverage'], columns=['Scores'])
+
+    # Print dataframe in tabular format
+    print(tabulate(df_score, headers='keys', tablefmt='pretty'))
+    print(tabulate(df_average, headers='keys', tablefmt='pretty'))
 
 def update_lexicon(df):
     path = './'
@@ -231,6 +308,7 @@ def test_lexicon(df):
     del df_predict['Sentiment']
 
     df_predict = label(df_predict)
+
     return accuracy(df_truth, df_predict)
 
 df_train = pd.read_pickle('./isk_train.pkl')
@@ -239,5 +317,7 @@ df_test = pd.read_pickle('./isk_test.pkl')
 del df_test['id']
 df_unlabeled = pd.read_pickle('./tuning_isk.pkl')
 
-cleaned_df = process_dataframe(df_test)
+cleaned_df = process_dataframe(df_train)
 update_lexicon(cleaned_df)
+
+test_lexicon(df_test)
