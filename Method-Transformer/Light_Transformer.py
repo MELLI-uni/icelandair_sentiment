90fd0d07ee73efef0a80e553da1d547034006f89
MELLI <jae0228@gmail.com>
2022-08-07 22:45:44 -0500

    edits

--- Method-Transformer/Light_Transformer.py
+++ Method-Transformer/Light_Transformer.py
@@ -57,39 +57,38 @@ class biLSTM(torch.nn.Module):
                  bidirectional, dropout, pad_idx):
         
         super().__init__()
-
+        
         self.embedding = torch.nn.Embedding(vocab_size, embedding_dim, padding_idx = pad_idx)
-
-        self.encoder = torch.nn.LSTM(embedding_dim, 
-                               hidden_dim, 
-                               num_layers=n_layers,
-                               bidirectional=bidirectional,
-                               dropout=dropout)
-
-        self.predictor = torch.nn.Linear(hidden_dim*2, output_dim)
-
+        
+        self.rnn = torch.nn.LSTM(embedding_dim, 
+                           hidden_dim, 
+                           num_layers=n_layers, 
+                           bidirectional=bidirectional, 
+                           dropout=dropout)
+        
+        self.fc = torch.nn.Linear(hidden_dim * 2, output_dim)
+        
         self.dropout = torch.nn.Dropout(dropout)
-      
+        
     def forward(self, text, text_lengths):
-        embedded = self.dropout(self.embedding(text))    
+        embedded = self.dropout(self.embedding(text))
+
         packed_embedded = torch.nn.utils.rnn.pack_padded_sequence(embedded, text_lengths.to('cpu'))
-        packed_output, (hidden, cell) = self.encoder(packed_embedded)
+        
+        packed_output, (hidden, cell) = self.rnn(packed_embedded)
+
         output, output_lengths = torch.nn.utils.rnn.pad_packed_sequence(packed_output)
-
         hidden = self.dropout(torch.cat((hidden[-2,:,:], hidden[-1,:,:]), dim = 1))
-
-        return self.predictor(hidden)
+            
+        return self.fc(hidden)
 
 def categorical_accuracy(preds, y):
-    """
-    Returns accuracy per batch, i.e. if you get 8/10 right, this returns 0.8, NOT 8
-    """
     top_pred = preds.argmax(1, keepdim = True)
     correct = top_pred.eq(y.view_as(top_pred)).sum()
     acc = correct.float() / y.shape[0]
     return acc
 
-def train(model, iterator, optimizer, criterion):
+def CNN_train(model, iterator, optimizer, criterion):
     
     epoch_loss = 0
     epoch_acc = 0
@@ -115,7 +114,7 @@ def train(model, iterator, optimizer, criterion):
         
     return epoch_loss / len(iterator), epoch_acc / len(iterator)
 
-def evaluate(model, iterator, criterion):
+def CNN_evaluate(model, iterator, criterion):
     
     epoch_loss = 0
     epoch_acc = 0
@@ -205,11 +204,147 @@ def test_CNN(json_file, lang):
     N_EPOCHS = 5
 
     for epoch in range(N_EPOCHS):
+        train_loss, train_acc = CNN_train(model, train_iterator, optimizer, criterion)
+        print(f'\tTrain Loss: {train_loss:.3f} | Train Acc: {train_acc*100:.2f}%')
+
+    test_loss, test_acc = CNN_evaluate(model, test_iterator, criterion)
+    print(test_acc)
+
+
+def binary_accuracy(preds, y):
+    """
+    Returns accuracy per batch, i.e. if you get 8/10 right, this returns 0.8, NOT 8
+    """
+
+    #round predictions to the closest integer
+    rounded_preds = torch.round(torch.sigmoid(preds))
+    correct = (rounded_preds == y).float() #convert into float for division 
+    acc = correct.sum() / len(correct)
+    return acc
+
+def train(model, iterator, optimizer, criterion):
+    
+    epoch_loss = 0
+    epoch_acc = 0
+    
+    model.train()
+    
+    for batch in iterator:
+        
+        optimizer.zero_grad()
+        
+        text, text_lengths = batch.text
+        
+        predictions = model(text, text_lengths).squeeze(1)
+        
+        loss = criterion(predictions, batch.label)
+        
+        acc = binary_accuracy(predictions, batch.label)
+        
+        loss.backward()
+        
+        optimizer.step()
+        
+        epoch_loss += loss.item()
+        epoch_acc += acc.item()
+        
+    return epoch_loss / len(iterator), epoch_acc / len(iterator)
+
+def evaluate(model, iterator, criterion):
+    
+    epoch_loss = 0
+    epoch_acc = 0
+    
+    model.eval()
+    
+    with torch.no_grad():
+    
+        for batch in iterator:
+
+            text, text_lengths = batch.text
+            
+            predictions = model(text, text_lengths).squeeze(1)
+            
+            loss = criterion(predictions, batch.label)
+            
+            acc = binary_accuracy(predictions, batch.label)
+
+            epoch_loss += loss.item()
+            epoch_acc += acc.item()
+        
+    return epoch_loss / len(iterator), epoch_acc / len(iterator)
+
+def test_biLSTM(json_file, lang):
+    TEXT = data.Field(tokenize = 'spacy',
+                  tokenizer_language = 'en_core_web_sm',
+                  include_lengths = True)
+
+    LABEL = data.LabelField(dtype = torch.float)
+
+    fields = {'answer_freetext_value': ('text', TEXT), 'Sentiment': ('label', LABEL)}
+
+    dataset = torchtext.legacy.data.TabularDataset(
+        path=json_file,
+        format="json",
+        fields=fields)
+
+    (train_data, test_data) = dataset.split(split_ratio=[0.8,0.2])
+
+    MAX_VOCAB_SIZE = 25_000
+
+    TEXT.build_vocab(
+            train_data, 
+            max_size = MAX_VOCAB_SIZE, 
+            vectors = 'glove.6B.100d',
+            unk_init = torch.Tensor.normal_)
+
+    LABEL.build_vocab(
+            train_data)
+
+    train_iterator, test_iterator = data.BucketIterator.splits(
+        (train_data, test_data),
+        device = device,
+        batch_size = BATCH_SIZE,
+        sort_key = lambda x: len(x.text),
+        sort_within_batch = True)
+
+    INPUT_DIM = len(TEXT.vocab)
+    EMBEDDING_DIM = 100
+    HIDDEN_DIM = 256
+    OUTPUT_DIM = 1
+    N_LAYERS = 2
+    BIDIRECTIONAL = True
+    DROPOUT = 0.5
+    PAD_IDX = TEXT.vocab.stoi[TEXT.pad_token]
+
+    model = biLSTM(INPUT_DIM, 
+                EMBEDDING_DIM, 
+                HIDDEN_DIM, 
+                OUTPUT_DIM, 
+                N_LAYERS, 
+                BIDIRECTIONAL, 
+                DROPOUT, 
+                PAD_IDX)
+
+    pretrained_embeddings = TEXT.vocab.vectors
+    model.embedding.weight.data.copy_(pretrained_embeddings)
+
+    UNK_IDX = TEXT.vocab.stoi[TEXT.unk_token]
+
+    model.embedding.weight.data[UNK_IDX] = torch.zeros(EMBEDDING_DIM)
+    model.embedding.weight.data[PAD_IDX] = torch.zeros(EMBEDDING_DIM)
+
+    optimizer = optim.Adam(model.parameters())
+    criterion = torch.nn.BCEWithLogitsLoss()
+
+    model = model.to(device)
+    criterion = criterion.to(device)
+
+    N_EPOCHS = 5
+
+    for epoch in range(N_EPOCHS):       
         train_loss, train_acc = train(model, train_iterator, optimizer, criterion)
         print(f'\tTrain Loss: {train_loss:.3f} | Train Acc: {train_acc*100:.2f}%')
 
     test_loss, test_acc = evaluate(model, test_iterator, criterion)
     print(test_acc)
-
-def test_biLSTM(json_file, lang):
-    print("biLSTM")
