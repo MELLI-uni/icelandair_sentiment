61863e947886610d7b27b311014e55c5ed9ff0d1
MELLI <jae0228@gmail.com>
2022-08-08 14:45:01 -0500

    editting

--- Method-Transformer/backup.py
+++ Method-Transformer/backup.py
@@ -1,7 +1,14 @@
 import torch
-from torchtext.legacy import data
+import torch.nn as nn
+import torch.optim as optim
 
-SEED = 1234
+import random
+import time
+
+from torchtext.legacy import data
+from torchtext.legacy import datasets
+
+SEED = 99
 
 torch.manual_seed(SEED)
 torch.backends.cudnn.deterministic = True
@@ -12,12 +19,7 @@ TEXT = data.Field(tokenize = 'spacy',
 
 LABEL = data.LabelField(dtype = torch.float)
 
-from torchtext.legacy import datasets
-
 train_data, test_data = datasets.IMDB.splits(TEXT, LABEL)
-
-import random
-
 train_data, valid_data = train_data.split(random_state = random.seed(SEED))
 
 MAX_VOCAB_SIZE = 25_000
@@ -38,8 +40,6 @@ train_iterator, valid_iterator, test_iterator = data.BucketIterator.splits(
     sort_within_batch = True,
     device = device)
 
-import torch.nn as nn
-
 class RNN(nn.Module):
     def __init__(self, vocab_size, embedding_dim, hidden_dim, output_dim, n_layers, 
                  bidirectional, dropout, pad_idx):
@@ -59,35 +59,14 @@ class RNN(nn.Module):
         self.dropout = nn.Dropout(dropout)
         
     def forward(self, text, text_lengths):
-        
-        #text = [sent len, batch size]
-        
         embedded = self.dropout(self.embedding(text))
-        
-        #embedded = [sent len, batch size, emb dim]
-        
-        #pack sequence
-        # lengths need to be on CPU!
         packed_embedded = nn.utils.rnn.pack_padded_sequence(embedded, text_lengths.to('cpu'))
         
         packed_output, (hidden, cell) = self.rnn(packed_embedded)
-        
-        #unpack sequence
-        output, output_lengths = nn.utils.rnn.pad_packed_sequence(packed_output)
 
-        #output = [sent len, batch size, hid dim * num directions]
-        #output over padding tokens are zero tensors
-        
-        #hidden = [num layers * num directions, batch size, hid dim]
-        #cell = [num layers * num directions, batch size, hid dim]
-        
-        #concat the final forward (hidden[-2,:,:]) and backward (hidden[-1,:,:]) hidden layers
-        #and apply dropout
-        
+        output, output_lengths = nn.utils.rnn.pad_packed_sequence(packed_output)
         hidden = self.dropout(torch.cat((hidden[-2,:,:], hidden[-1,:,:]), dim = 1))
-                
-        #hidden = [batch size, hid dim * num directions]
-            
+
         return self.fc(hidden)
 
 INPUT_DIM = len(TEXT.vocab)
@@ -108,14 +87,7 @@ model = RNN(INPUT_DIM,
             DROPOUT, 
             PAD_IDX)
 
-def count_parameters(model):
-    return sum(p.numel() for p in model.parameters() if p.requires_grad)
-
-print(f'The model has {count_parameters(model):,} trainable parameters')
-
 pretrained_embeddings = TEXT.vocab.vectors
-
-print(pretrained_embeddings.shape)
 
 model.embedding.weight.data.copy_(pretrained_embeddings)
 
@@ -124,10 +96,6 @@ UNK_IDX = TEXT.vocab.stoi[TEXT.unk_token]
 model.embedding.weight.data[UNK_IDX] = torch.zeros(EMBEDDING_DIM)
 model.embedding.weight.data[PAD_IDX] = torch.zeros(EMBEDDING_DIM)
 
-print(model.embedding.weight.data)
-
-import torch.optim as optim
-
 optimizer = optim.Adam(model.parameters())
 criterion = nn.BCEWithLogitsLoss()
 
@@ -135,11 +103,6 @@ model = model.to(device)
 criterion = criterion.to(device)
 
 def binary_accuracy(preds, y):
-    """
-    Returns accuracy per batch, i.e. if you get 8/10 right, this returns 0.8, NOT 8
-    """
-
-    #round predictions to the closest integer
     rounded_preds = torch.round(torch.sigmoid(preds))
     correct = (rounded_preds == y).float() #convert into float for division 
     acc = correct.sum() / len(correct)
@@ -197,33 +160,12 @@ def evaluate(model, iterator, criterion):
         
     return epoch_loss / len(iterator), epoch_acc / len(iterator)
 
-import time
-
-def epoch_time(start_time, end_time):
-    elapsed_time = end_time - start_time
-    elapsed_mins = int(elapsed_time / 60)
-    elapsed_secs = int(elapsed_time - (elapsed_mins * 60))
-    return elapsed_mins, elapsed_secs
-
 N_EPOCHS = 5
 
-best_valid_loss = float('inf')
-
 for epoch in range(N_EPOCHS):
-
-    start_time = time.time()
-    
     train_loss, train_acc = train(model, train_iterator, optimizer, criterion)
-    valid_loss, valid_acc = evaluate(model, valid_iterator, criterion)
     
-    end_time = time.time()
-
-    epoch_mins, epoch_secs = epoch_time(start_time, end_time)
-    
-    print(f'Epoch: {epoch+1:02} | Epoch Time: {epoch_mins}m {epoch_secs}s')
     print(f'\tTrain Loss: {train_loss:.3f} | Train Acc: {train_acc*100:.2f}%')
-    print(f'\t Val. Loss: {valid_loss:.3f} |  Val. Acc: {valid_acc*100:.2f}%')
 
 test_loss, test_acc = evaluate(model, test_iterator, criterion)
-
 print(f'Test Loss: {test_loss:.3f} | Test Acc: {test_acc*100:.2f}%')
